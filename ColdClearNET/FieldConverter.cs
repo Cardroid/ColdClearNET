812278febe4440e01c26b121c33340611a7de38b
CarbonSIX <number1dh@gmail.com>
2020-06-02 02:49:15 +0900

    Add Field Converter

---
+++ ColdClearNET/FieldConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColdClearNET
+{
+    public class FieldConverter
+    {
+        public static bool[] Converter(int[,] intfield)
+        {
+            if (!(intfield.GetLength(0) == 40 && intfield.GetLength(1) == 10))
+                throw new IndexOutOfRangeException("intfield is not int[40, 10]");
+
+            bool[] boolfield = new bool[400];
+            int count = 0;
+            
+            for (int i = 0; i < intfield.GetLength(0); i++)
+            {
+                for (int j = 0; j < intfield.GetLength(1); j++)
+                {
+                    boolfield[count] = intfield[i, j] > 0 ? true : false;
+                    count++;
+                }
+            }
+
+            return boolfield;
+        }
+    }
+}
