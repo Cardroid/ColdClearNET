4a4a9d80874cae57021131c3df0d0c5994c69faa
CarbonSIX <number1dh@gmail.com>
2020-06-06 09:14:43 +0900

    .gitignore update

---
+++ ColdClearNET/Converter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColdClearNET
+{
+    public class Converter
+    {
+        public static bool[] FieldConverter(int[,] intfield)
+        {
+            bool[] boolfield = new bool[400];
+            int count = 0;
+
+            for (int j = 0; j < intfield.GetLength(1); j++)
+            {
+                for (int i = 0; i < intfield.GetLength(0); i++)
+                {
+                    boolfield[count++] = intfield[i, j] >= 0;
+                }
+            }
+
+            return boolfield;
+        }
+
+        public static CCPiece PieceConverter(Piece pptpiece)
+        {
+            CCPiece ccPiece = CCPiece.CC_S;
+            switch (pptpiece)
+            {
+                case Piece.S:
+                    ccPiece = CCPiece.CC_S;
+                    break;
+                case Piece.Z:
+                    ccPiece = CCPiece.CC_Z;
+                    break;
+                case Piece.J:
+                    ccPiece = CCPiece.CC_J;
+                    break;
+                case Piece.L:
+                    ccPiece = CCPiece.CC_L;
+                    break;
+                case Piece.T:
+                    ccPiece = CCPiece.CC_T;
+                    break;
+                case Piece.O:
+                    ccPiece = CCPiece.CC_O;
+                    break;
+                case Piece.I:
+                    ccPiece = CCPiece.CC_I;
+                    break;
+            }
+            return ccPiece;
+        }
+    }
+
+    public enum Piece
+    {
+        S, Z, J, L, T, O, I
+    }
+}
